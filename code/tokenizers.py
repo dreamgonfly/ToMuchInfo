8106bee56575b546ffd637904dce57f832b0f428
dreamgonfly <dreamgonfly@gmail.com>
2018-04-04 15:05:05 +0900

    JamoTokenizer Added

--- code/tokenizers.py
+++ code/tokenizers.py
@@ -1,3 +1,5 @@
+from jamo import h2j, j2hcj
+import re
 
 class DummyTokenizer:
     """A dummy tokenizer that splits a sentence by space"""
@@ -18,3 +20,20 @@ class DummyTokenizer:
         """
         
         return raw_text.split()
+
+
+class JamoTokenizer:
+    """Split text into jamo's"""
+
+    def __init__(self, config):
+        self.movie_actor = re.compile(r"mv[0-9]*|ac[0-9]*|.")
+
+    def tokenize(self, raw_text):
+        """
+
+        :param raw_text: "아이들이 보면 좋을 mv00217576~"
+        :return: ['ㅇ', 'ㅏ', 'ㅇ', 'ㅣ', 'ㄷ', 'ㅡ', 'ㄹ', 'ㅇ', 'ㅣ', ' ', 'ㅂ', 'ㅗ', 'ㅁ', 'ㅕ', 'ㄴ', ' ', 'ㅈ', 'ㅗ', 'ㅎ', 'ㅇ', 'ㅡ', 'ㄹ', ' ', 'mv00217576', '~']
+
+        """
+        jamo_text = j2hcj(h2j(raw_text))
+        return self.movie_actor.findall(jamo_text)
