50ad4ca82e113b974d5942075ca339746ea76ef2
angrypark <sungnam1108@naver.com>
2018-04-15 01:30:57 +0900

    angrypark) 마지막이 되길 바랍니다.

--- code/models_config.py
+++ code/models_config.py
@@ -25,7 +25,7 @@ MODELS_CONFIG = {
     'DCNN_LSTM_oh_my_god': {
         'model': 'DCNN_LSTM',
         'normalizer': 'BasicNormalizer',
-	'tokenizer': 'SoyNLPTokenizer',
+	    'tokenizer': 'SoyNLPTokenizer',
         'dictionary': 'RandomDictionary',
         'vocabulary_size': 100000,
         'shuffle_dataset': True,
@@ -33,7 +33,17 @@ MODELS_CONFIG = {
         'learning_rate': 0.001,
         'min_length': 16,
         'max_length': 100,
-        'embedding_size': 300 
-
+        'embedding_size': 300
+    },
+    'WordCNN_Soynlp': {
+        'model': 'WordCNN',
+        'normalizer': 'BasicNormalizer',
+        'tokenizer': 'SoyNLPTokenizer',
+        'dictionary': 'RandomDictionary',
+        'learning_rate': 0.005,
+        'shuffle_dictionary': True,
+        'lr_schedule': True,
+        'embedding_size': 300,
+    },
     }
 }
