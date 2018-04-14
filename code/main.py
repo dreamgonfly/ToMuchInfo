8375b146d3f13faab9f56269fe4e5a3b7d2e2b34
skdy33 <skdy3333@gmail.com>
2018-04-14 22:32:19 +0900

    added my main

--- code/main.py
+++ code/main.py
@@ -8,20 +8,23 @@ from torch.autograd import Variable
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
+import models
+import normalizers
 import tokenizers
 import feature_extractors
 import dictionaries
 from dataloader import load_data, collate_fn, Preprocessor, MovieReviewDataset
 from trainers import Trainer
 import utils
+from LSUV import LSUVinit
 
 import nsml
 from nsml import DATASET_PATH, HAS_DATASET, GPU_NUM, IS_ON_NSML
 
-from models.WordCNN import WordCNN
-from models.VDCNN import VDCNN
-from models.LSTMClassifier import LSTMClassifier
-from models.CNN_LSTM import CNN_LSTM
+#hparams
+LOCAL_TRAIN_DIR = 'data/movie_review_phase1/'
+EMBEDDING_REQUIRES_GRAD = True # False if uses fasttext or word2vec embeddings
+
 # Random seed
 np.random.seed(0)
 torch.manual_seed(0)
@@ -33,11 +36,12 @@ args.add_argument('--pause', type=int, default=0)
 args.add_argument('--iteration', type=str, default='0')
 
 # User options
-args.add_argument('--model', type=str, default='WordCNN', choices=['WordCNN', 'VDCNN','LSTMClassifier','CNN_LSTM'])
-args.add_argument('--tokenizer', type=str, default='DummyTokenizer', choices=['JamoTokenizer', 'JamoMaskedTokenizer','DummyTokenizer','TwitterTokenizer'])
+args.add_argument('--model', type=str, default='WordCNN')
+args.add_argument('--normalizer', type=str, default='DummyNormalizer')
+args.add_argument('--tokenizer', type=str, default='JamoTokenizer')
 args.add_argument('--features', type=str, default='LengthFeatureExtractor')  # LengthFeatureExtractor_MovieActorFeaturesExtractor ...
-args.add_argument('--dictionary', type=str, default='RandomDictionary', choices=['RandomDictionary', 'FasttextDictionary'])
-args.add_argument('--use_gpu', type=bool, default= False)
+args.add_argument('--dictionary', type=str, default='RandomDictionary')
+args.add_argument('--use_gpu', type=bool, default=torch.cuda.is_available() or GPU_NUM)
 args.add_argument('--output', type=int, default=1)
 args.add_argument('--epochs', type=int, default=10)
 args.add_argument('--batch_size', type=int, default=64)
@@ -51,15 +55,16 @@ args.add_argument('--learning_rate', type=float, default=0.01)
 args.add_argument('--lr_schedule', action='store_true')
 args.add_argument('--print_every', type=int, default=1)
 args.add_argument('--save_every', type=int, default=1)
+args.add_argument('--requires_grad', type=bool, default=EMBEDDING_REQUIRES_GRAD)
 config = args.parse_args()
 
 logger = utils.get_logger('MovieReview')
 logger.info('Arguments: {}'.format(config))
 
-if config.model == 'WordCNN': Model = WordCNN
-elif config.model == 'VDCNN': Model = VDCNN
-elif config.model == 'LSTMClassifier': Model = LSTMClassifier
-elif config.model == 'CNN_LSTM': Model = CNN_LSTM
+Model = getattr(models, config.model)
+
+Normalizer = getattr(normalizers, config.normalizer)
+normalizer = Normalizer(config)
 
 Tokenizer = getattr(tokenizers, config.tokenizer)
 tokenizer = Tokenizer(config)
@@ -73,14 +78,16 @@ for feature_name in config.features.split('_'):
     feature_extractor = FeatureExtractor(config)
     feature_extractor_list.append((feature_name, feature_extractor))
 
-preprocessor = Preprocessor(tokenizer, feature_extractor_list, dictionary)
+preprocessor = Preprocessor(config, normalizer, tokenizer, feature_extractor_list, dictionary)
+print("Number of features:", preprocessor.n_features)
 
-model = Model(config)
+model = Model(config, n_features=preprocessor.n_features)
+
 if config.use_gpu:
     model = model.cuda()
 
 if not HAS_DATASET and not IS_ON_NSML:  # It is not running on nsml
-    DATASET_PATH = 'data/movie_review_phase1/'
+    DATASET_PATH = LOCAL_TRAIN_DIR
 
 # DONOTCHANGE: They are reserved for nsml
 # This is for nsml leaderboard
@@ -91,13 +98,6 @@ def bind_model(model, config):
             'model': model.state_dict(),
             'preprocessor': preprocessor.state_dict()
         }
-        torch.save(checkpoint, filename)
-
-    # 저장한 모델을 불러올 수 있는 함수입니다.
-    def load(filename, *args):
-        checkpoint = torch.load(filename)
-        model.load_state_dict(checkpoint['model'])
-        preprocessor.load_state_dict(checkpoint['preprocessor'])
 
         print('Model loaded')
 
@@ -161,6 +161,26 @@ if config.mode == 'train':
     criterion = nn.MSELoss()
     trainable_params = [p for p in model.parameters() if p.requires_grad]
     optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=config.learning_rate)
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.8)  # .ReduceLROnPlateau(optimizer, factor=0.7, patience=5, min_lr=0.00005)
+
+    trainer = Trainer(model, train_dataloader, val_dataloader, criterion=criterion, optimizer=optimizer,
+                      lr_schedule=config.lr_schedule, lr_scheduler=lr_scheduler, use_gpu=config.use_gpu, logger=logger)
+    trainer.run(epochs=config.epochs)
+
+# 로컬 테스트 모드일때 사용합니다
+# 결과가 아래와 같이 나온다면, nsml submit을 통해서 제출할 수 있습니다.
+# [(0.0, 9.045), (0.0, 5.91), ... ]
+elif config.mode == 'test_local':
+    with open(os.path.join(DATASET_PATH, 'train/train_data'), 'rt', encoding='utf-8') as f:
+        reviews = f.readlines()
+    res = nsml.infer(reviews)
+    print(res)
+import argparse
+import os
+from os.path import dirname, abspath, join, exists
+import numpy as np
+
+    optimizer = optim.Adam(params=trainable_params, lr=config.learning_rate)
     lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.8)  # .ReduceLROnPlateau(optimizer, factor=0.7, patience=5, min_lr=0.00005)
 
     trainer = Trainer(model, train_dataloader, val_dataloader, criterion=criterion, optimizer=optimizer,
