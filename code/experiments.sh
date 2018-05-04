0d6e070f63465a9cc2012230178c6fdca608509d
angrypark <angrypark@Sungnamui-iMac.local>
2018-05-04 11:29:19 +0900

    angrypark) delete unncessary files

--- code/experiments.sh
+++
@@ -1,1 +0,0 @@
-python main.py --mode=test_local --model=VDCNN --tokenizer=JamoTokenizer --dictionary=RandomDictionary --epochs=100 --sort_dataset --lr_schedule --learning_rate=0.002 --min_length=64 --max_length=100
